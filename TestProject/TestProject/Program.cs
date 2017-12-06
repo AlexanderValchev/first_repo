5fb0060bb27ec77810573cb6475954fee93de1c1
Alexander Valchev <alexandervalchev88@gmail.com>
2017-12-06 13:04:13 +0200

    Changed the Hello string

--- TestProject/TestProject/Program.cs
+++ TestProject/TestProject/Program.cs
@@ -10,7 +10,7 @@ namespace TestProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello GitHub!");
+            Console.WriteLine("Hello from Git & GitHub!");
         }
     }
 }
